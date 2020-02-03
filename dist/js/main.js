21c4a8a934ec98fffcbba8fd9754a3a66b3ac8bf
adekpoh9424 <42923599+adekpoh9424@users.noreply.github.com>
2020-02-02 19:00:54 -0600

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,13 +4,13 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
-// Set Initual State Of Menu
+// Set Initial State Of Menu
 
 let showMenu = false;
 
-menu.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
